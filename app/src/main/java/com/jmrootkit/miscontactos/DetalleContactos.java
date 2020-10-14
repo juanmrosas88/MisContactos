8acd1ff391d328410490c977aefbfbcbbfd9a7ab
juanm <juanmrosas8@gmail.com>
2020-10-14 20:05:07 -0300

    Nueva activity_detalle_contactos.xml y java nuevo icono caledario

---
+++ app/src/main/java/com/jmrootkit/miscontactos/DetalleContactos.java
@@ -0,0 +1,14 @@
+package com.jmrootkit.miscontactos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DetalleContactos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detalle_contactos);
+    }
+}
--- app/src/main/java/com/jmrootkit/miscontactos/MainActivity.java
+++ app/src/main/java/com/jmrootkit/miscontactos/MainActivity.java
@@ -2,7 +2,9 @@ package com.jmrootkit.miscontactos;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.DatePicker;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,5 +18,10 @@ public class MainActivity extends AppCompatActivity {
         // init
         // dpDate.init(2002, 10, 27, null);
 
+    }
+
+    public void next_onClick(View view) {
+        Intent i = new Intent(MainActivity.this, DetalleContactos.class);
+        startActivity(i);
     }
 }
